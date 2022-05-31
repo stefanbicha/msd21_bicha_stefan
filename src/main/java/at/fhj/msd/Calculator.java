8ac2cd5890ad7e529f7d0090d90456c8295e6e8e
Stefan Bicha <stefan.bicha@edu.fh-joanneum.at>
2022-05-31 09:56:03 +0200

    added branch and javaDocs for Calculator.java

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -9,15 +9,46 @@ public class Calculator {
 
     }
 
+    /**
+     * returns number1 plus number2
+     *
+     * @param number1 the first number
+     * @param number2 the second number
+     * @return
+     */
     public static double add(double number1, double number2){
         return number1 + number2;
     }
+
+    /**
+     * returns number1 minus number2
+     *
+     * @param number1 the first number
+     * @param number2 the second number
+     * @return
+     */
     public static double minus(double number1, double number2){
         return number1 - number2;
     }
+
+    /**
+     * returns number1 diveded by number2
+     *
+     * @param number1 the first number
+     * @param number2 the second number
+     * @return
+     */
     public static double divide(double number1, double number2){
         return number1 == 0 ? null : number1 / number2;
     }
+
+    /**
+     * returns number1 multiplyed with number2
+     *
+     * @param number1 the first number
+     * @param number2 the second number
+     * @return
+     */
     public static double multiply(double number1, double number2){
         return number1 * number2;
     }
